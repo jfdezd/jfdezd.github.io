0f8407eb56057b6480860e2e00fe29be1e9751f1
jfdezd <javierfernandezdominguez@gmail.com>
2022-05-23 20:34:56 +0200

    Updates

---
+++ precache-manifest.7169cabfd6f69ba77b2bae5d434f1429.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d4a7e2da5c870664e02dcf4961ae54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd240dfe8b71c9b93c38",
+    "url": "/static/css/main.512c1981.chunk.css"
+  },
+  {
+    "revision": "880fc39f72873e300e82",
+    "url": "/static/js/2.4078e6b4.chunk.js"
+  },
+  {
+    "revision": "bd240dfe8b71c9b93c38",
+    "url": "/static/js/main.6af9964c.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
